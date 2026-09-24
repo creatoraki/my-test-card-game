1d1bf395760675cefa2e4978f797f924c0575b9c
蔡智鹏 <405673777@qq.com>
2026-09-24 13:04:31 +0800

    1321

--- src/dev/TestScreen/TestScreen.tsx
+++ src/dev/TestScreen/TestScreen.tsx
@@ -1,11 +1,9 @@
-import { PlatformerDemo } from "@/dev/PlatformerDemo";
 import s from "./TestScreen.module.css";
 
 // 测试页: 仅开发环境通过 ?page=test 进入。当前挂载生态方舟 2D 横版跳跃演示。
 export function TestScreen() {
   return (
     <main className={s.root}>
-      <PlatformerDemo />
     </main>
   );
 }
